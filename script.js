dd721336b47c24b95b321a43cc28b75735a2b084
Maverick177 <130297908+Gaby177@users.noreply.github.com>
2025-12-25 23:52:43 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -90,7 +90,7 @@ function renderStage(stage) {
             break;
 
         case STAGES.START:
-            const startBtn = createButton('Apasa-ma', () => {
+            const startBtn = createButton('Continua', () => {
                 // Clear the content area (buttons) immediately
                 contentArea.innerHTML = '';
                 saveState(STAGES.INTRO);
@@ -341,7 +341,7 @@ function playTone(freq, duration = 0.3) {
     osc.type = 'sine'; // retro sound
     osc.frequency.setValueAtTime(freq, audioCtx.currentTime);
 
-    gain.gain.setValueAtTime(0.1, audioCtx.currentTime);
+    gain.gain.setValueAtTime(0.5, audioCtx.currentTime);
     gain.gain.exponentialRampToValueAtTime(0.0001, audioCtx.currentTime + duration);
 
     osc.connect(gain);
@@ -357,12 +357,15 @@ function runIntroSequence() {
     contentArea.classList.add('fade-in');
 
     const dialogues = [
-        { speaker: 'Unknown', text: 'Bine ai venit. Numele meu este G (apasa pentru a continua)' },
-        { speaker: 'Profesor G', text: 'In ultima vreme am fost preocupat de o cercetare extrem de importanta.' },
-        { speaker: 'Profesor G', text: 'Un virus misterios creste de la o zi la alta, infiltrandu-se in tot spatiul virtual.' },
-        { speaker: 'Profesor G', text: 'Daca nu este oprit, internetul asa cum il stim ar putea fi infectat.' },
-        { speaker: 'Profesor G', text: 'Pentru a-l opri... am nevoie de ajutorul tau.' },
-        { speaker: 'Profesor G', text: 'Esti pregatita sa pornesti in aceasta aventura?' }
+        { speaker: 'Unknown', text: 'In ultima vreme am fost preocupat de o cercetare extrem de importanta. (apasa pentru a continua)' },
+        { speaker: 'Unknown', text: 'Un specimen informatic care promitea imbunatatirea sigurantei paginilor web' },
+        { speaker: 'Unknown', text: 'Inca nu stiu de ce, dar dezvoltarea lui nu a iesit cum era planuit ' },
+        { speaker: 'Unknown', text: 'S-a transformat intr-un virus informatic, si a scapat din capsula in care era stocat' },
+        { speaker: 'Unknown', text: 'Cu greu am reusit sa-l prind si sa-l inchid la loc.' },
+        { speaker: 'Unknown', text: 'Insa a lasat in urma lui pagube care, daca nu sunt remediate...' },
+        { speaker: 'Unknown', text: 'ameninta sa distruga website-uri intregi.' },
+        { speaker: 'Unknown', text: 'Am nevoie de ajutorul tau pentru a le identifica si  elimina.' },
+        { speaker: 'Unknown', text: 'Esti pregatita sa pornesti in aceasta misiune?' }
     ];
     playDialogueSequence(dialogues, () => {
         saveState(STAGES.CHOICE);
@@ -378,8 +381,8 @@ function showChoiceScreen() {
         controlsArea.innerHTML = '';
 
         const d = [
-            { speaker: 'Profesor G', text: 'Pentru a salva internetul, am nevoie de timpul si atentia ta.' },
-            { speaker: 'Profesor G', text: 'Apasa butonul urmator doar atunci cand esti pregatita.' }
+            { speaker: 'Unknown', text: 'Pentru a salva internetul, am nevoie de timpul si atentia ta.' },
+            { speaker: 'Unknown', text: 'Apasa butonul urmator atunci cand esti pregatita.' }
         ];
         playDialogueSequence(d, () => {
             saveState(STAGES.READY_WAIT);
@@ -402,8 +405,8 @@ function showChoiceScreen() {
 function showMissionAborted() {
     controlsArea.innerHTML = '';
     const d = [
-        { speaker: 'Profesor G', text: 'Inteleg... Uneori, cele mai importante misiuni raman neincepute.' },
-        { speaker: 'Profesor G', text: 'Iti doresc mult succes in drumul tau.' }
+        { speaker: 'Unknown', text: 'Inteleg... Uneori, cele mai importante misiuni raman neincepute.' },
+        { speaker: 'Unknown', text: 'Iti doresc mult succes in drumul tau.' }
     ];
     playDialogueSequence(d, () => {
         hideDialogue();
@@ -444,19 +447,20 @@ function runStage1() {
     hideDialogue();
 
     const d = [
-        { speaker: 'Profesor G', text: 'Super, ma bucur ca ai ales sa incepi aceasta aventura.' },
+        { speaker: 'Unknown', text: 'Super, ma bucur ca ai ales sa accepti aceasta misiune.' },
+        { speaker: 'Unknown', text: 'Imi cer scuze, inca nu m-am prezentat:' },
+        { speaker: 'Unknown', text: 'Numele meu este G. Cunoscut ca profesor G.' },
         { speaker: 'Profesor G', text: 'Ce trebuie sa stii despre acest virus este ca e foarte siret.' },
-        { speaker: 'Profesor G', text: 'Se ascunde prin diferite website-uri pentru a fi greu de gasit, si infecteaza tot ce prinde.' },
-        { speaker: 'Profesor G', text: 'Din fericire am un dipozitiv care ma ajuta sa ii iau urma.' },
-        { speaker: 'Profesor G', text: 'Insa am nevoie de ajutorul tau pentru a gasi indiciile.' },
+        { speaker: 'Profesor G', text: 'Din fericire am un dipozitiv care ma ajuta sa identific website-urile unde a produs pagube.' },
         { speaker: 'Profesor G', text: 'Sa incepem...' },
-        { speaker: 'Profesor G', text: 'Aparatul imi arata ca initial virusul a trecut prin Google Fotos.' },
-        { speaker: 'Profesor G', text: 'A gasit o imagine in care a ascuns un obiect malitios.' },
-        { speaker: 'Profesor G', text: 'Am reusit sa identific imaginea, dar nu si obiectul.' },
+        { speaker: 'Profesor G', text: 'Aparatul imi arata ca initial virusul a trecut pe la Google Fotos.' },
+        { speaker: 'Profesor G', text: 'A gasit o imagine in care a ascuns un obiect periculos.' },
         { speaker: 'Profesor G', text: 'Trebuie neaparat sa gasim acel obiect, de altfel toate pozele vor fi infectate.' },
         { speaker: 'Profesor G', text: 'Nu-i asa ca nu vrei sa pierzi toate acele amintiri frumoase?' },
-        { speaker: 'Profesor G', text: 'Tot ce stiu este ca acel obiect este o cheie.' },
-        { speaker: 'Profesor G', text: 'Vezi daca poti sa identifici la ce coordonate se afla cheia.' }
+        { speaker: 'Profesor G', text: 'Am reusit sa identific imaginea, dar nu si obiectul.' },
+        { speaker: 'Profesor G', text: 'Este necesar sa aflam coordonatele obiectului ascuns.' },
+        { speaker: 'Profesor G', text: 'Tot ce stiu este ca a lasat urmatoarea ghicitoare:' },
+        { speaker: 'Profesor G', text: 'Acest obiect nu functioneaza singur, dar fara el nimic nu se deschide. In ce casuta se afla?' }
     ];
 
     playDialogueSequence(d, () => {
@@ -474,7 +478,7 @@ function runStage1() {
                 saveState(STAGES.STAGE_2);
                 renderStage(STAGES.STAGE_2);
             } else {
-                showFeedback('Cheia nu este acolo... mai incearca.', true)
+                showFeedback('Obiectul nu este acolo... mai incearca.', true)
             }
         });
         contentArea.appendChild(btn);
@@ -484,24 +488,22 @@ function runStage1() {
 function runStage2() {
     hideDialogue();
     const d = [
-        { speaker: 'Profesor G', text: 'Excelent, ai reusit sa identifici cheia.' },
-        { speaker: 'Profesor G', text: 'Acum pot sa elimin infectia lasata si sa salvez toate pozele.' },
-        { speaker: 'Profesor G', text: 'Nu doar ca m-ai ajutat sa salvez acele amintiri frumoase,' },
-        { speaker: 'Profesor G', text: 'Ci ai gasit urmatorul indiciu.' },
-        { speaker: 'Profesor G', text: 'Se pare ca urmaorul obiectiv a fost wikipedia.com' },
-        { speaker: 'Profesor G', text: 'A lasat si acolo un mic virus.' },
-        { speaker: 'Profesor G', text: 'Daca il lasam acolo, va altera toate definițiile.' },
-        { speaker: 'Profesor G', text: 'S-a ascuns fix in locul unde nu te-ai aștepta...' },
-        { speaker: 'Profesor G', text: '...chiar în definiția lui.' },
-        { speaker: 'Profesor G', text: 'Poti sa ma ajuti sa identific definitia corecta?' }
+        { speaker: 'Profesor G', text: 'Excelent, ai reusit sa identifici obiectul, care era defapt o cheie.' },
+        { speaker: 'Profesor G', text: 'Acum pot sa o elimin si sa salvez toate pozele.' },
+        { speaker: 'Profesor G', text: 'M-ai ajutat sa salvez acele amintiri frumoase pe care le stocam in poze.' },
+        { speaker: 'Profesor G', text: 'Se pare ca urmatorul obiectiv a fost Wikipedia.com' },
+        { speaker: 'Profesor G', text: 'Acolo a ascuns o diversiune.' },
+        { speaker: 'Profesor G', text: 'Daca nu o identificam, va altera toate definitiile.' },
+        { speaker: 'Profesor G', text: 'A ascuns-o chiar in definitia lui. ' },
+        { speaker: 'Profesor G', text: 'Poti sa identifici definitia corecta?' }
     ];
     playDialogueSequence(d, () => {
         contentArea.innerHTML = '';
 
         const defs = [
-            { text: 'Un virus este un microorganism microscopic care face parte din categoria organismelor unicelulare și care se reproduce prin diviziune celulară...', correct: false },
-            { text: 'Un virus este un agent infecțios microscopic, alcătuit din material genetic (ADN sau ARN) învelit într-o capsidă proteică, care se poate multiplica doar în interiorul celulelor vii ale unui organism gazdă.', correct: true },
-            { text: 'Un virus este o bacterie extrem de mică, invizibilă cu microscopul optic obișnuit...', correct: false }
+            { text: 'Un virus este un microorganism microscopic care face parte din categoria organismelor unicelulare si care se reproduce prin diviziune celulara...', correct: false },
+            { text: 'Un virus este un agent infectios microscopic, alcătuit din material genetic (ADN sau ARN) invelit într-o capsida proteica, care se poate multiplica doar in interiorul celulelor vii ale unui organism gazda.', correct: true },
+            { text: 'Un virus este o bacterie extrem de mica, invizibila cu microscopul optic obisnuit...', correct: false }
         ];
 
         const container = document.createElement('div');
@@ -532,13 +534,14 @@ function runStage3() {
     // Split dialogue to insert game
     const d1 = [
         { speaker: 'Profesor G', text: 'Minunat, ai reusit sa identifici definitia corecta.' },
-        { speaker: 'Profesor G', text: 'Am eliminat acum micul virus lasat acolo.' },
+        { speaker: 'Profesor G', text: 'Acum am eliminat complet diversiunea.' },
         { speaker: 'Profesor G', text: 'Este foarte important ca definitiile sa ramana corecte.' },
-        { speaker: 'Profesor G', text: 'Sa trecem la urmatorul pas.' },
-        { speaker: 'Profesor G', text: 'Aparatul imi indica acum ca virusul a trecut apoi pe imdb.com.' },
-        { speaker: 'Profesor G', text: 'A vizitat animatia Klaus. O fi virus… dar trebuie să recunosc, are gusturi bune :D' },
-        { speaker: 'Profesor G', text: 'Klaus e o animație excelentă, ți-o recomand cu drag dacă n-ai apucat s-o vezi.' },
-        { speaker: 'Profesor G', text: 'Se pare ca aici a manipulat annul de lansare pentru niste filme' },
+        { speaker: 'Profesor G', text: 'Sa mergem mai departe..' },
+        { speaker: 'Profesor G', text: 'Aparatul imi indica acum ca virusul a trecut apoi pe la Imdb.com.' },
+        { speaker: 'Profesor G', text: 'A vizitat animatia Klaus.' },
+        { speaker: 'Profesor G', text: 'O fi virus dar trebuie sa recunosc, are gusturi bune :D' },
+        { speaker: 'Profesor G', text: 'Klaus e o animatie excelenta, ti-o recomand cu drag daca nu ai vazut-o inca.' },
+        { speaker: 'Profesor G', text: 'Se pare ca aici a sters anul de lansare al unor filme' },
         { speaker: 'Profesor G', text: 'Daca nu le punem la loc, aceste filme risca sa dispara.' },
         { speaker: 'Profesor G', text: 'Vezi daca poti sa identifici anii de lansare pentru filmele urmatoare:' }
     ];
@@ -600,11 +603,10 @@ function runStage4() {
     const d = [
         { speaker: 'Profesor G', text: 'Foarte bine, ai identificat corect datelele de lansare.' },
         { speaker: 'Profesor G', text: 'Ar fi fost pacat ca aceste filme sa fi disparut.' },
-        { speaker: 'Profesor G', text: 'Sa continuam cu următoarea urmă: spotify.com' },
-        { speaker: 'Profesor G', text: 'Ce bine că am ales pe cineva cu ureche muzicală.' },
+        { speaker: 'Profesor G', text: 'Sa continuam cu urmatorul obiectiv al virusului: Spotify.com' },
         { speaker: 'Profesor G', text: 'Aici a lasat o bariera - un mic puzzle pe care trebuie sa-l rezolvi.' },
-        { speaker: 'Profesor G', text: 'Pentru a continua sa ii luam urma, am nevoie de tine.' },
-        { speaker: 'Profesor G', text: 'Poti identifica pattern-ul melodiei? (P.S este nevoie de sunet)' }
+        { speaker: 'Profesor G', text: 'Pentru a continua sa ii luam urma, am nevoie de urechea ta muzicala.' },
+        { speaker: 'Profesor G', text: 'Poti identifica pattern-ul melodiei? (P.S O sa ai nevoie de sunet)' }
     ];
     playDialogueSequence(d, () => {
         contentArea.innerHTML = '';
@@ -676,11 +678,11 @@ function checkMusicInput(note) {
 function runStage5() {
     hideDialogue();
     const d = [
-        { speaker: 'Profesor G', text: 'Bravo! Stiam ca nu o sa ai probleme cu acest puzzle.' },
-        { speaker: 'Profesor G', text: 'Ca tot veni vorba de muzica, cu siguranta ca ti s-a mai spus ca ai o voce frumoasa' },
+        { speaker: 'Profesor G', text: 'Bravo! Eram sigur ca nu o sa ai probleme cu acest puzzle.' },
+        { speaker: 'Profesor G', text: 'Stiam ca ai ureche muzicala si cu siguranta ca ti s-a mai spus ca ai o voce frumoasa' },
         { speaker: 'Profesor G', text: 'Continua sa dezvolti aceasta calitate pe care o ai. ' },
-        { speaker: 'Profesor G', text: 'Bun, sa continuam urmarirea virusului nostru.' },
-        { speaker: 'Profesor G', text: 'Din datele mele, virusul a trecut și pe la mybible.eu.' },
+        { speaker: 'Profesor G', text: 'Bun, sa continuam cu misiunea noastra.' },
+        { speaker: 'Profesor G', text: 'Din datele mele, virusul a trecut si pe la MyBible.eu.' },
         { speaker: 'Profesor G', text: 'Vad ca a sters referinta unui verset, si trebuie sa o punem la loc' },
         { speaker: 'Profesor G', text: 'Reusesti sa gasesti referinta versetului?' }
     ];
@@ -761,15 +763,14 @@ function runStage6() {
     hideDialogue();
     const d = [
         { speaker: 'Profesor G', text: 'L-ai gasit!' },
-        { speaker: 'Profesor G', text: 'Chiar este un verset frumos' },
-        { speaker: 'Profesor G', text: 'In acest an asta iti doresc:' },
-        { speaker: 'Profesor G', text: 'Domnul sa fie cu tine in toate si sa iti dea izbanda,' },
-        { speaker: 'Profesor G', text: 'sa te pazeasca si sa te umple cu toate binecuvantarile Sale.' },
+        { speaker: 'Profesor G', text: 'Ma bucur, deoarece chiar este un verset frumos' },
+        { speaker: 'Profesor G', text: 'Sper sa il pastrezi in inima ta anul acesta.' },
+        { speaker: 'Profesor G', text: 'Domnul sa te pazeasca si sa te umple cu toate binecuvantarile Sale.' },
         { speaker: 'Profesor G', text: 'Bun...' },
-        { speaker: 'Profesor G', text: 'Sa continuam cu urmarirea virusului nostru.' },
-        { speaker: 'Profesor G', text: 'Urmatorul indiciu pare a se gasi pe Youtube.com' },
+        { speaker: 'Profesor G', text: 'Sa continuam cu virusul nostru.' },
+        { speaker: 'Profesor G', text: 'Urmatorul website pe pe care l-a corupt este Youtube.com' },
         { speaker: 'Profesor G', text: 'Observ ca a trecut pe la un videoclip muzical si a sters niste cuvinte din versuri.' },
-        { speaker: 'Profesor G', text: 'Sunt convins ca am ales persoana potrivita' },
+        { speaker: 'Profesor G', text: 'Sunt convins ca esti persoana potrivita' },
         { speaker: 'Profesor G', text: 'Stiu ca vei gasi cu usurinta cuvintele lipsa' },
         { speaker: 'Profesor G', text: 'Iata versurile:' }
     ];
@@ -802,7 +803,7 @@ function runStage6() {
                 saveState(STAGES.STAGE_7);
                 renderStage(STAGES.STAGE_7);
             } else {
-                showFeedback('Nu cred ca sunt corecte....', true);
+                showFeedback('Ceva nu e bine...', true);
             }
         });
         btn.style.marginTop = '20px';
@@ -815,12 +816,11 @@ function runStage7() {
     const d = [
         { speaker: 'Profesor G', text: 'Perfect, nici nu imi faceam griji ca nu vei reusi :)' },
         { speaker: 'Profesor G', text: 'Foarte frumoasa melodia, cat si interpretarea ;)' },
-        { speaker: 'Profesor G', text: 'Acum ca am sa reparam dauna, mergem mai departe.' },
-        { speaker: 'Profesor G', text: 'Aparatul meu imi indica ultimul indiciu: Google Maps' },
-        { speaker: 'Profesor G', text: 'Cred ca aici o sa il prindem.' },
-        { speaker: 'Profesor G', text: 'S-a ascuns in locatia din urmatoarea poza' },
-        { speaker: 'Profesor G', text: 'Ca sa il prindem trebuie sa stiu numele strazii din acea locatie' },
-        { speaker: 'Profesor G', text: 'Am un presentiment ca vei stii numele strazii:' }
+        { speaker: 'Profesor G', text: 'Acum ca am reparat dauna, mergem mai departe.' },
+        { speaker: 'Profesor G', text: 'Aparatul meu indica ultimul website corupt: Google Maps' },
+        { speaker: 'Profesor G', text: 'Am reusit sa gasesc locatia unde a creat o bresa.' },
+        { speaker: 'Profesor G', text: 'Ca sa inchid bresa trebuie sa stiu numele strazii unde a fost creata' },
+        { speaker: 'Profesor G', text: 'Am un presentiment ca vei stii numele acestei strazi:' }
     ];
 
     playDialogueSequence(d, () => {
@@ -835,7 +835,7 @@ function runStage7() {
 
         const btn = createButton('Verifica', () => {
             const v = inp.value.trim().toLowerCase();
-            const valid = ['calle de rusia', 'calle rusia', 'c. rusia'].includes(v);
+            const valid = ['calle de rusia', 'calle rusia', 'c. rusia', 'c. de rusia', 'c rusia', 'c de rusia'].includes(v);
 
             if (valid) {
                 saveState(STAGES.FINAL);
@@ -851,11 +851,11 @@ function runStage7() {
 function runFinal() {
     hideDialogue();
     const d = [
-        { speaker: 'Profesor G', text: 'Minunat, asta era numele strazii!' },
+        { speaker: 'Profesor G', text: 'Perfect, asta era numele strazii!' },
         { speaker: 'Profesor G', text: 'Acum imi aduc si eu aminte:' },
-        { speaker: 'Profesor G', text: 'Am petrecut cativa ani la soala de aici, ce amintiri fumoase...' },
-        { speaker: 'Profesor G', text: 'In sfarsit l-am prins. Acum definitiv.' },
-        { speaker: 'Profesor G', text: 'Iti multumesc pentru tot ajutorul tau.' },
+        { speaker: 'Profesor G', text: 'Am petrecut cativa ani in acest loc, ce amintiri fumoase...' },
+        { speaker: 'Profesor G', text: 'In sfarsit reparat toate daunele lasate in urma.' },
+        { speaker: 'Profesor G', text: 'Iti multumesc pentru tot ajutorul oferit.' },
         { speaker: 'Profesor G', text: 'Acum pot sta linistit stiind ca internetul este salvat.' },
         { speaker: 'Profesor G', text: 'Mult succes in viitor, si fereste-te de orice fel de virusi :))' }
     ];
